0ee948f2ded0dec295660dc2548746988beb93c9
ronamit <ronamit@campus.technnion.ac.il>
2022-12-27 14:22:04 +0200

    up

--- generate_dataset.py
+++ generate_dataset.py
@@ -24,28 +24,27 @@ if not os.path.isdir(dataset_out_path):
 seq_paths = glob.glob(sim_out_path + '/*/')
 print(seq_paths)
 
-
-# camera intrinsic parameters
-focal_length = 4.969783  # [millimeter]
-sensor_width = 10.26  # [millimeter]
-sensor_height = 7.695  # [millimeter]
-cols = 320  # [pixels] image width
-rows = 240  # [pixels]  image height
-fx = focal_length * cols / sensor_width  # focal length in x-axis [pixels]
-fy = focal_length * rows / sensor_height  # focal length in y-axis [pixels]
-cx = cols / 2.0  # middle of the image in x-axis [pixels]
-cy = rows / 2.0  # middle of the image in y-axis [pixels]
-
-# Create video from frame sequence
 for seq_in_path in seq_paths:
     seq_name = os.path.basename(os.path.normpath(seq_in_path))
     seq_out_path = os.path.join(dataset_out_path, seq_name)
     if not os.path.isdir(seq_out_path):
         os.makedirs(seq_out_path)
-    shutil.copy2(os.path.join(seq_in_path, 'MySettings.set'), os.path.join(seq_out_path + 'M-ySettings.set'))
+    shutil.copy2(os.path.join(seq_in_path, 'MySettings.set'), os.path.join(seq_out_path, 'MySettings.set'))
+
+    # camera intrinsic parameters
+    focal_length = 4.969783  # [millimeter]
+    sensor_width = 10.26  # [millimeter]
+    sensor_height = 7.695  # [millimeter]
+    cols = 320  # [pixels] image width
+    rows = 240  # [pixels]  image height
+    fx = focal_length * cols / sensor_width  # focal length in x-axis [pixels]
+    fy = focal_length * rows / sensor_height  # focal length in y-axis [pixels]
+    cx = cols / 2.0  # middle of the image in x-axis [pixels]
+    cy = rows / 2.0  # middle of the image in y-axis [pixels]
 
     frame_rate = 20  # shotPerSec":"float(20)
 
+    # Create video from frame sequence
     frames2video(images_path=seq_in_path,
                  ffmpeg_path=args.ffmpeg_path,
                  seq_out_path=seq_out_path,
--- util.py
+++ util.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 import glob
 
+
 def frames2video(images_path, ffmpeg_path, seq_out_path, vid_file_name, frame_rate,
                  output_format='mp4'):
     '''
@@ -13,12 +14,11 @@ def frames2video(images_path, ffmpeg_path, seq_out_path, vid_file_name, frame_ra
 
     output_file = os.path.join(seq_out_path, vid_file_name) + '.' + output_format
 
-    codec = 'libx264'
     command = [ffmpeg_path, '-y',
                '-framerate', str(frame_rate),
                '-i', image_prefix + '%05d.png',
-               '-vcodec', codec,
+               '-vcodec', 'libx265',  # codec
+               '-crf', '15',   # compression vs. quality factor - see https://trac.ffmpeg.org/wiki/Encode/H.265
                output_file]
     subprocess.run(command)
-    # Note: '-i' is a workaround (https://stackoverflow.com/questions/31201164/ffmpeg-error-pattern-type-glob-was-selected-but-globbing-is-not-support-ed-by)
     print(f'Video saved to: {output_file}')
