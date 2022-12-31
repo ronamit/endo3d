e7124fece54d79fe6624fc1b1f6fa31a0e9dabc0
ronamit <ronamit@campus.technnion.ac.il>
2022-12-31 15:12:54 +0200

    up

--- plot_frame.py
+++ plot_frame.py
@@ -5,7 +5,6 @@ from util import get_frame_at_timestamp
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ffmpeg_path', type=str, required=True, help='path to ffmpeg executable')
     parser.add_argument('--dataset_path', type=str, required=True,
                         help=' path to  the dataset to be loaded')
     parser.add_argument('--seq_name', type=str, required=True,
